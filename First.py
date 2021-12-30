9fd0d16cae2a976148e61d111dd808c9ac2857b3
Vidya Shagar RS <vidyashagarvs.3@gmail.com>
2021-12-30 11:48:18 +0530

    done

---
+++ First.py
@@ -0,0 +1,23 @@
+# (Variable and String concatenating)
+
+name= input("What is your Name? ")
+color= input("what is your favourite color? ")
+print(name+" like "+color)
+
+# (Variable Type Conversion)
+
+weight_in_pounds= input("What is your weight in pounds? ")
+weight_in_kg= float(weight_in_pounds) * 0.453592
+print("Your weight in KG is:")
+print(weight_in_kg)
+
+# (Long Message String)
+
+message= '''Hello all,
+Its just
+  an
+Example
+ long
+message'''
+print(message)
+
