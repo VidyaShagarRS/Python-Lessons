24fa858e0d1e78753c9da8f097043083c549ce1c
VidyaShagarRS <96609010+VidyaShagarRS@users.noreply.github.com>
2022-01-11 14:44:41 +0530

    Add files via upload

---
+++ Fundamentals.py
@@ -0,0 +1,12 @@
+# # (If Else - Control Statements)
+#
+# mark = 65
+#
+# if (mark >= 90):
+#     print("Excellent")
+# elif (mark >=75):
+#     print("Good")
+# elif (mark >=50):
+#     print("Average")
+# else:
+#     print("Oops, You're Fail")
